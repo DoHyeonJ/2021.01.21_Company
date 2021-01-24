05f7832a3206df9faf60f355eae409d7947e9978
DohyeonJ <jdh7693@naver.com>
2021-01-24 15:40:25 +0900

    add fragments

--- src/main/java/main/company/home/HomeController.java
+++ src/main/java/main/company/home/HomeController.java
@@ -1,5 +1,6 @@
 package main.company.home;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,5 +11,5 @@ public class HomeController {
     public String home() {
         return "/index";
     }
-
+    
 }
