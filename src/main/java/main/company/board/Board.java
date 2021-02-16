6fb0ed86178f02cccaa76d4cd875805826bb491e
dohyeonj <jdh7693@naver.com>
2021-02-17 00:03:04 +0900

    add board

---
+++ src/main/java/main/company/board/Board.java
@@ -0,0 +1,28 @@
+package main.company.board;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Board {
+
+    @Id @GeneratedValue
+    private Long idx;
+
+    @Column
+    private String subject;
+
+    @Column
+    private String content;
+
+    @Column
+    private String name;
+
+    @Column
+    private LocalDateTime register;
+
+}
